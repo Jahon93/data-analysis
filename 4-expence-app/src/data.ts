3d8ecaed9363a06fea39949d438892b8ba99d588
Jakhongir Iskandarov <55333953+Jahon93@users.noreply.github.com>
2022-12-13 17:12:56 +0900

    nest project created

---
+++ 4-expence-app/src/data.ts
@@ -0,0 +1,8 @@
+// {
+//   "id": "uuid3",
+//   "source": "Salary",
+//   "amount": 7000,
+//   "created_At": "2022-12-13T06:44:31.992Z",
+//   "updated_At": "2022-12-13T06:44:31.992Z",
+//   "type": "expense"
+// }
